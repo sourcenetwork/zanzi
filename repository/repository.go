06c8c6c04b11459675badad537e3198d06d3216f
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-11-03 12:25:12 -0300

    refactor: Change TupleRepository to use generic

--- repository/repository.go
+++ repository/repository.go
@@ -5,29 +5,27 @@ import (
 	"fmt"
 
 	"github.com/sourcenetwork/source-zanzibar/model"
+	"github.com/sourcenetwork/source-zanzibar/utils"
 )
 
 // TupleRepository abstract interfacing with tuple storage.
-//
-// Contract: methods should return instance of `EntityNotFound`
-// when some operation failed because the lookup resulted in an empty set.
-type TupleRepository interface {
+type TupleRepository[T model.TupleRecord] interface {
 	// Store a new tuple. Update all indexes
-	SetTuple(tuple model.Tuple) (model.TupleRecord, error)
+	SetTuple(tuple T) error
 
 	// Looks up a relation tuple from the backend storage
-	GetTuple(tuple model.Tuple) (model.TupleRecord, error)
+	GetTuple(object, subject model.AuthNode) (utils.Option[T], error) // TODO this may be kinda annoying, having two auth nodes without an intermediary type
 
 	// Return tuples directly connected to userset
 	// Does not follows userset indirection.
-	GetRelatedUsersets(userset model.Userset) ([]model.TupleRecord, error)
+	GetRelatedUsersets(node model.AuthNode) ([]T, error)
 
 	// Return tuples that have an outgoing edge to userset (aka reverse lookup)
 	// Does not perform userset chasing
-	GetIncomingUsersets(userset model.Userset) ([]model.TupleRecord, error)
+	GetIncomingUsersets(node model.AuthNode) ([]T, error)
 
 	// Purge tuple from storage.
-	RemoveTuple(tuple model.Tuple) error
+	RemoveTuple(object, subject model.AuthNode) error
 
 	// Return all tuples that match the filter:
 	// tuple.Relation == relation
@@ -35,7 +33,7 @@ type TupleRepository interface {
 	//
 	// This peculiar query is used to reverse tuple to userset rules
 	// during reverse lookup
-	GetTuplesFromRelationAndUserObject(relation string, objNamespace string, objectId string) ([]model.TupleRecord, error)
+	GetTuplesFromRelationAndUserObject(relation string, objNamespace string, objectId string) ([]T, error)
 }
 
 // NamespaceRepository abstract interfacing with namespace storage.
@@ -43,7 +41,7 @@ type TupleRepository interface {
 // Contract: methods should return instance of `EntityNotFound`
 // when some operation failed because the lookup resulted in an empty set.
 type NamespaceRepository interface {
-	GetNamespace(namespace string) (model.Namespace, error)
+	GetNamespace(namespace string) (types.Option[model.Namespace], error)
 
 	SetNamespace(namespace model.Namespace) (model.Namespace, error)
 
@@ -55,37 +53,3 @@ type NamespaceRepository interface {
 	// Return all relations which reference the given `relation`.
 	GetReferrers(namespace, relation string) ([]model.Relation, error)
 }
-
-// EntityNotFound type implements error interface.
-// Caller code may compare against it to verify whether
-// returned error was a storage error or a not found error
-type EntityNotFound struct {
-	Entity string
-	Args   []any
-}
-
-// Build instance of EntityNotFound.
-// Caller may specify entity name and any filters used during
-// lookup
-// Supplied information will be used to generate final error message.
-func NewEntityNotFound(entity string, args ...any) error {
-	return &EntityNotFound{
-		Entity: entity,
-		Args:   args,
-	}
-}
-
-func (e *EntityNotFound) Error() string {
-	return fmt.Sprintf("Entity not found: entity=%v, args=%v", e.Entity, e.Args)
-}
-
-func (e *EntityNotFound) Is(target error) bool {
-	switch target.(type) {
-	case *EntityNotFound:
-		return true
-	default:
-		return false
-	}
-}
-
-var _ error = (*EntityNotFound)(nil)
