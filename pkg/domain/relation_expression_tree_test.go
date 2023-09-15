ceac8a237dbaad8d14f01ae4b96fe2f7a340d74d
Bruno Gomes <gomes.bruno.ac@gmail.com>
2023-09-15 15:04:06 -0300

    feat: mapper

---
+++ pkg/domain/relation_expression_tree_test.go
@@ -0,0 +1,55 @@
+package domain
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/require"
+)
+
+func TestThisToRelationExpression(t *testing.T) {
+    tree := ThisNode()
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "_this")
+}
+
+func TestComputedUsersetToRelationExpression(t *testing.T) {
+    tree := CUNode("relation")
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "relation")
+}
+
+func TestTupleToUsersetToRelationExpression(t *testing.T) {
+    tree := TTUNode("parent", "owner")
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "parent->owner")
+}
+
+func TestUnionToRelationExpression(t *testing.T) {
+    tree := UnionNode(CUNode("left"), CUNode("right"))
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "(left + right)")
+}
+
+func TestIntersectionToRelationExpression(t *testing.T) {
+    tree := IntersectionNode(CUNode("left"), CUNode("right"))
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "(left & right)")
+}
+
+func TestDifferenceToRelationExpression(t *testing.T) {
+    tree := DifferenceNode(CUNode("left"), CUNode("right"))
+
+    relExpr := tree.RelationExpression()
+
+    require.Equal(t, relExpr, "(left - right)")
+}
