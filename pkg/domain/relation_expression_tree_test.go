739f6b1943a59fc3915096fa52ad80ac09c123e4
Bruno Gomes <gomes.bruno.ac@gmail.com>
2023-09-15 15:04:18 -0300

    style: format

--- pkg/domain/relation_expression_tree_test.go
+++ pkg/domain/relation_expression_tree_test.go
@@ -1,55 +1,55 @@
 package domain
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
 func TestThisToRelationExpression(t *testing.T) {
-    tree := ThisNode()
+	tree := ThisNode()
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "_this")
+	require.Equal(t, relExpr, "_this")
 }
 
 func TestComputedUsersetToRelationExpression(t *testing.T) {
-    tree := CUNode("relation")
+	tree := CUNode("relation")
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "relation")
+	require.Equal(t, relExpr, "relation")
 }
 
 func TestTupleToUsersetToRelationExpression(t *testing.T) {
-    tree := TTUNode("parent", "owner")
+	tree := TTUNode("parent", "owner")
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "parent->owner")
+	require.Equal(t, relExpr, "parent->owner")
 }
 
 func TestUnionToRelationExpression(t *testing.T) {
-    tree := UnionNode(CUNode("left"), CUNode("right"))
+	tree := UnionNode(CUNode("left"), CUNode("right"))
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "(left + right)")
+	require.Equal(t, relExpr, "(left + right)")
 }
 
 func TestIntersectionToRelationExpression(t *testing.T) {
-    tree := IntersectionNode(CUNode("left"), CUNode("right"))
+	tree := IntersectionNode(CUNode("left"), CUNode("right"))
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "(left & right)")
+	require.Equal(t, relExpr, "(left & right)")
 }
 
 func TestDifferenceToRelationExpression(t *testing.T) {
-    tree := DifferenceNode(CUNode("left"), CUNode("right"))
+	tree := DifferenceNode(CUNode("left"), CUNode("right"))
 
-    relExpr := tree.RelationExpression()
+	relExpr := tree.RelationExpression()
 
-    require.Equal(t, relExpr, "(left - right)")
+	require.Equal(t, relExpr, "(left - right)")
 }
